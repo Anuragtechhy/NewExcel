b678cb941a57bf4b0206d4d79146699f0f1af8fe
Dell <Dell@DESKTOP-G77TL2P>
2025-03-01 00:21:08 +0530

    new commit

---
+++ Excelsheet/src/excelplease/Excel.java
@@ -0,0 +1,29 @@
+package excelplease;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Excel {
+
+    private static final Logger log = LogManager.getLogger(Excel.class);
+
+    public static void main(String[] args) {
+
+    	
+        // Log information
+        log.info("Starting WebDriver");
+
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Dell\\Chromedriver-new\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();   
+
+        log.info("Opening Google...");
+        driver.get("https://www.google.com/");
+        
+        log.info("Google opened successfully");
+
+        driver.quit();
+        log.info("Browser closed");
+    }
+}
