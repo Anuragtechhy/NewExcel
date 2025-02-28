3f0e1e726866e8dfe8bf5de47259515268eda22a
Dell <Dell@DESKTOP-G77TL2P>
2025-03-01 00:26:53 +0530

    test branch commit

--- Excelsheet/src/excelplease/Excel.java
+++ Excelsheet/src/excelplease/Excel.java
@@ -9,19 +9,22 @@ public class Excel {
 
     private static final Logger log = LogManager.getLogger(Excel.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
     	
         // Log information
         log.info("Starting WebDriver");
 
         System.setProperty("webdriver.chrome.driver","C:\\Users\\Dell\\Chromedriver-new\\chromedriver-win64\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();   
-
+        WebDriver driver = new ChromeDriver();  
+        
+        driver.manage().window().maximize();
         log.info("Opening Google...");
         driver.get("https://www.google.com/");
         
         log.info("Google opened successfully");
+        
+        Thread.sleep(2500);
 
         driver.quit();
         log.info("Browser closed");
